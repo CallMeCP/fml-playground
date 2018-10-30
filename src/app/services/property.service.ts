d687bdb81af80dddf8c1c8bb2fd9c0038a09224f
CPL <chiaping_5@hotmail.com>
2018-10-30 09:44:43 +0800

    Clean up comments

--- src/app/services/property.service.ts
+++ src/app/services/property.service.ts
@@ -1001,6 +1001,14 @@ export class PropertyService {
             addedLblComment = true;
           }
 
+          // Align left, center, right
+          // x=0 align=left || x=width/2 align=center || y=width align=right
+          // when reverse back, right is left in CSS, and left is right in CSS.
+
+          // Valign top, center, bottom
+          // y=0 valign=top || y=height/2 valign=center || y=height valign=bottom
+          // when reverse back, bottom is top is CSS, and top is bottom in CSS.
+
           // String for alignments
           const hrztAlign: string = lbl.horizontalAlign;
           const vAlign: string = lbl.verticalAlign;
@@ -1022,24 +1030,13 @@ export class PropertyService {
             alignStr += `y=${lbl.height*PP} valign=bottom`;
           }
 
-          // const str: string =
-            finalFmlStr += `\t\t<t x=${(lbl.x-bodyX)*PP} y=${(lbl.y-bodyY)*PP} w=${lbl.width*PP} h=${lbl.height*PP} bgcol=${lbl.bgColor} col=${lbl.fontColor} font=${lbl.fontFamily} sz=${lbl.fontSize*PP}>\n`;
-            finalFmlStr += `\t\t\t<t w=${lbl.width*PP} h=${lbl.height*PP} ${alignStr}>\n`;
-            finalFmlStr += `\t\t\t\t${lbl.bold?'<bo>': ''}${lbl.italic?'<i>':''}${lbl.content}${lbl.italic?'</i>': ''}${lbl.bold?'</bo>':''}\n`;
-            finalFmlStr += `\t\t\t</t>\n`;
-            finalFmlStr += `\t\t</t>\n`;
-            finalFmlStr += `\n`;
-
-
-            // Align left, center, right
-            // x=0 align=left || x=width/2 align=center || y=width align=right
-            // when reverse back, right is left in CSS, and left is right in CSS.
-
-            // Valign top, center, bottom
-            // y=0 valign=top || y=height/2 valign=center || y=height valign=bottom
-            // when reverse back, bottom is top is CSS, and top is bottom in CSS.
-
-          // finalFmlStr += str;
+          finalFmlStr += `\t\t<t x=${(lbl.x-bodyX)*PP} y=${(lbl.y-bodyY)*PP} w=${lbl.width*PP} h=${lbl.height*PP} bgcol=${lbl.bgColor} col=${lbl.fontColor} font=${lbl.fontFamily} sz=${lbl.fontSize*PP}>\n`;
+          finalFmlStr += `\t\t\t<t w=${lbl.width*PP} h=${lbl.height*PP} ${alignStr}>\n`;
+          finalFmlStr += `\t\t\t\t${lbl.bold?'<bo>': ''}${lbl.italic?'<i>':''}${lbl.content}${lbl.italic?'</i>': ''}${lbl.bold?'</bo>':''}\n`;
+          finalFmlStr += `\t\t\t</t>\n`;
+          finalFmlStr += `\t\t</t>\n`;
+          finalFmlStr += `\n`;
+            
         }
       });
 
